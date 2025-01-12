ce91f303fd7d1a4a5dd9fb4052cc47d1ef019c40
Tamy <tamy8677@gmail.com>
2025-01-12 14:31:22 +0800

    建立披薩清單頁面

---
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+// PizzaList.cshtml.cs隨附的 PageModel 類別
+
+namespace ContosoPizza.Pages
+{
+    public class PizzaListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
